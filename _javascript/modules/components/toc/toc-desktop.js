a1acf6425a289f699a7b3d68d24a6a1e3c680096
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-05-01 20:58:20 +0900

    update

--- _javascript/modules/components/toc/toc-desktop.js
+++ _javascript/modules/components/toc/toc-desktop.js
@@ -5,6 +5,7 @@ export class TocDesktop {
     contentSelector: '.content',
     ignoreSelector: '[data-toc-skip]',
     headingSelector: 'h2, h3, h4',
+    collapseDepth: 0,
     orderedList: false,
     scrollSmooth: false,
     headingsOffset: 16 * 2 // 2rem
